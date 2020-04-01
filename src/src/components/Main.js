da17e9896d1a8c6d5e8bed635e7d98ddb8423ebc
Tim Paine <t.paine154@gmail.com>
2020-04-01 14:59:29 -0400

    .

--- src/src/components/Main.js
+++ src/src/components/Main.js
@@ -157,162 +157,203 @@ class Main extends Component {
       renderer.setSize( window.innerWidth, window.innerHeight );
       this.mount.appendChild( renderer.domElement );
       
-      var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 500 );
-      camera.position.set( 0, 0, 250 );
-      camera.lookAt( window.innerWidth/20, -30, 0 );
-      
-      var scene = new THREE.Scene();
-
-      //create a blue MeshLineMaterial
-      // var material1 = new MeshLineMaterial( { color: 0xb0acb0 } );
-      // var material2 = new MeshLineMaterial( { color: 0xe2dddf } );
-      // var material3 = new MeshLineMaterial( { color: 0x85ebd9 } );
-      // var material4 = new MeshLineMaterial( { color: 0x3d898d } );
-      // var material5 = new MeshLineMaterial( { color: 0x2f404d } );
-      // var material6 = new MeshLineMaterial( { color: 0x00ffff } );
-      var material1 = new THREE.LineBasicMaterial( { color: 0xb0acb0 } );
-      var material2 = new THREE.LineBasicMaterial( { color: 0xe2dddf } );
-      var material3 = new THREE.LineBasicMaterial( { color: 0x85ebd9 } );
-      var material4 = new THREE.LineBasicMaterial( { color: 0x3d898d } );
-      var material5 = new THREE.LineBasicMaterial( { color: 0x2f404d } );
-      var material6 = new THREE.LineBasicMaterial( { color: 0x00ffff } );
-
+      // randomly generate walks
       var points1 = [];
       var points2 = [];
       var points3 = [];
       var points4 = [];
       var points5 = [];
       var points6 = [];
-      var walks = [
-        [[0, 0], ],
-        [[0, 0], ],
-        [[0, 0], ],
-        [[0, 0], ],
-        [[0, 0], ],
-        [[0, 0], ],
-      ];
-      var index = 0;
 
-      // var geometry1 = new THREE.Geometry().setFromPoints( points1 );
-      // var geometry2 = new THREE.Geometry().setFromPoints( points2 );
-      // var geometry3 = new THREE.Geometry().setFromPoints( points3 );
-      // var geometry4 = new THREE.Geometry().setFromPoints( points4 );
-      // var geometry5 = new THREE.Geometry().setFromPoints( points5 );
-      // var geometry6 = new THREE.Geometry().setFromPoints( points6 );
+      for(var i = 0; i<window.innerWidth; i++){
+        if(i === 0){
+          points1.push(new THREE.Vector3(-window.innerWidth/2, 100, 0));
+          points2.push(new THREE.Vector3(-window.innerWidth/2, 100, 0));
+          points3.push(new THREE.Vector3(-window.innerWidth/2, 100, 0));
+          points4.push(new THREE.Vector3(-window.innerWidth/2, 100, 0));
+          points5.push(new THREE.Vector3(-window.innerWidth/2, 100, 0));
+          points6.push(new THREE.Vector3(-window.innerWidth/2, 100, 0));
+        } else {
+          // construct line segements so we can simulate then walking segment-by-segment
+          points1.push(new THREE.Vector3(points1.slice(-1)[0].x+1, points1.slice(-1)[0].y + boxMullerRandom(), 0));
+          points1.push(new THREE.Vector3(points1.slice(-1)[0].x, points1.slice(-1)[0].y, 0));
+          points2.push(new THREE.Vector3(points2.slice(-1)[0].x+1, points2.slice(-1)[0].y + boxMullerRandom(), 0));
+          points2.push(new THREE.Vector3(points2.slice(-1)[0].x, points2.slice(-1)[0].y, 0));
+          points3.push(new THREE.Vector3(points3.slice(-1)[0].x+1, points3.slice(-1)[0].y + boxMullerRandom(), 0));
+          points3.push(new THREE.Vector3(points3.slice(-1)[0].x, points3.slice(-1)[0].y, 0));
+          points4.push(new THREE.Vector3(points4.slice(-1)[0].x+1, points4.slice(-1)[0].y + boxMullerRandom(), 0));
+          points4.push(new THREE.Vector3(points4.slice(-1)[0].x, points4.slice(-1)[0].y, 0));
+          points5.push(new THREE.Vector3(points5.slice(-1)[0].x+1, points5.slice(-1)[0].y + boxMullerRandom(), 0));
+          points5.push(new THREE.Vector3(points5.slice(-1)[0].x, points5.slice(-1)[0].y, 0));
+          points6.push(new THREE.Vector3(points6.slice(-1)[0].x+1, points6.slice(-1)[0].y + boxMullerRandom(), 0));
+          points6.push(new THREE.Vector3(points6.slice(-1)[0].x, points6.slice(-1)[0].y, 0));
+        }
+      }
+      // end generate random walks
+
+      // camera
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 2000);
+      camera.position.set( 0, 100, 800);
+      // camera.position.set( 0, 200, 200);
+      camera.lookAt( 0, 0, 0);
+      // end camera
+      
+      var scene = new THREE.Scene();
+
+      // Cube for reference
+      // var cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
+      // var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true, fog: false} );
+      // var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+      // scene.add( cube );
+      // end cube
+
+      // grid
+      var grid = new THREE.GridHelper( 2000, 100 );
+      grid.position.y = -100;
+      grid.position.z = -500;
+      scene.add( grid );
+      // fog to fade grid into "infinity"
+      scene.fog = new THREE.Fog( 0x000000, 1, 2000 );
+      // end grid
+
+      const vertexShader = `
+      precision mediump float;
+      precision mediump int;
+     
+      attribute vec4 color;
+      uniform float index;
+
+      varying vec4 vColor;
+      varying float vXValue;
+      varying float vIndex;
+     
+      void main()    {
+        vColor = color;
+        vIndex = index;
+        vXValue = position.x;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }`;
+
+      const fragmentShader = `
+      precision mediump float;
+      precision mediump int;
+     
+      varying vec4 vColor;
+      varying float vXValue;
+      varying float vIndex;
+     
+      void main()    {
+        vec4 color = vec4( vColor );
+        if(vXValue > vIndex){
+          gl_FragColor = vec4(255, 255, 255, 0);
+        } else {
+          gl_FragColor = color;
+        }
+      }
+      `;
+
+      var index = -(window.innerWidth/2);
+
+      var material = new THREE.ShaderMaterial({
+        vertexShader,
+        fragmentShader,
+        transparent: true,
+        uniforms: {
+          index: {value: index},
+        }
+      });
+
+      // TODO Color effects
+      var colors1 = new Float32Array(window.innerWidth * 4 * 2).fill(255);
+      var colors2 = new Float32Array(window.innerWidth * 4 * 2).fill(255);
+      var colors3 = new Float32Array(window.innerWidth * 4 * 2).fill(255);
+      var colors4 = new Float32Array(window.innerWidth * 4 * 2).fill(255);
+      var colors5 = new Float32Array(window.innerWidth * 4 * 2).fill(255);
+      var colors6 = new Float32Array(window.innerWidth * 4 * 2).fill(255);
+      for(var i = 0; i < colors1.length; i = i + 4){
+        colors1[i + 0] = 0;
+
+        colors2[i + 0] = 176/255;
+        colors2[i + 1] = 172/255;
+        colors2[i + 2] = 176/255;
+
+        colors3[i + 0] = 226/255;
+        colors3[i + 1] = 221/255;
+        colors3[i + 2] = 223/255;
+
+        colors4[i + 0] = 133/255;
+        colors4[i + 1] = 235/255;
+        colors4[i + 2] = 217/255;
+
+        colors5[i + 0] = 61/255;
+        colors5[i + 1] = 137/255;
+        colors5[i + 2] = 141/255;
+
+        colors6[i + 0] = 47/255;
+        colors6[i + 1] = 64/255;
+        colors6[i + 2] = 77/255;
+      }
+     
+      // load line geometries
       var geometry1 = new THREE.BufferGeometry().setFromPoints( points1 );
+      geometry1.setAttribute("color", new THREE.BufferAttribute(colors1, 4, true));
+
       var geometry2 = new THREE.BufferGeometry().setFromPoints( points2 );
+      geometry2.setAttribute("color", new THREE.BufferAttribute(colors2, 4, true));
+
       var geometry3 = new THREE.BufferGeometry().setFromPoints( points3 );
+      geometry3.setAttribute("color", new THREE.BufferAttribute(colors3, 4, true));
+
       var geometry4 = new THREE.BufferGeometry().setFromPoints( points4 );
+      geometry4.setAttribute("color", new THREE.BufferAttribute(colors4, 4, true));
+
       var geometry5 = new THREE.BufferGeometry().setFromPoints( points5 );
+      geometry5.setAttribute("color", new THREE.BufferAttribute(colors5, 4, true));
+
       var geometry6 = new THREE.BufferGeometry().setFromPoints( points6 );
+      geometry6.setAttribute("color", new THREE.BufferAttribute(colors6, 4, true));
 
+      // construct segments
+      var line1 = new THREE.LineSegments(geometry1, material);
+      var line2 = new THREE.LineSegments(geometry2, material);
+      // var line2 = new THREE.LineSegments(geometry2, material.clone());
+      var line3 = new THREE.LineSegments(geometry3, material);
+      var line4 = new THREE.LineSegments(geometry4, material);
+      var line5 = new THREE.LineSegments(geometry5, material);
+      var line6 = new THREE.LineSegments(geometry6, material);
 
-      // var _line1 = new MeshLine();
-      // _line1.setGeometry(geometry1);
-      // var line1 = new THREE.Mesh(_line1.geometry, material1);
+      // color
+      // line1.material.uniforms.color.value = [0, 255, 255, 255];
+      // line2.material.uniforms.color.value = [176, 172, 176, 255]; // b0acb0
+      // line3.material.uniforms.color.value = [226, 221, 223, 255]; // e2dddf
+      // line4.material.uniforms.color.value = [133, 235, 217, 255]; // 85ebd9
+      // line5.material.uniforms.color.value = [61, 137, 141, 255]; // 3d898d
+      // line1.material.uniforms.color.value = [47, 64, 77, 255]; // 2f404d
 
-      // var _line2 = new MeshLine();
-      // _line2.setGeometry(geometry2);
-      // var line2 = new THREE.Mesh(_line2.geometry, material2);
-
-      // var _line3 = new MeshLine();
-      // _line3.setGeometry(geometry1);
-      // var line3 = new THREE.Mesh(_line3.geometry, material3);
-
-      // var _line4 = new MeshLine();
-      // _line4.setGeometry(geometry1);
-      // var line4 = new THREE.Mesh(_line4.geometry, material4);
-
-      // var _line5 = new MeshLine();
-      // _line5.setGeometry(geometry1);
-      // var line5 = new THREE.Mesh(_line5.geometry, material5);
-
-      // var _line6 = new MeshLine();
-      // _line6.setGeometry(geometry1);
-      // var line6 = new THREE.Mesh(_line6.geometry, material6);
-
-      var line1 = new THREE.Line(geometry1, material1);
-      var line2 = new THREE.Line(geometry2, material2);
-      var line3 = new THREE.Line(geometry3, material3);
-      var line4 = new THREE.Line(geometry4, material4);
-      var line5 = new THREE.Line(geometry5, material5);
-      var line6 = new THREE.Line(geometry6, material6);
-
-
-
+      // add to scene
       scene.add( line1 );
       scene.add( line2 );
       scene.add( line3 );
       scene.add( line4 );
       scene.add( line5 );
       scene.add( line6 );
-      renderer.render( scene, camera );
-      // var add = 0.001;
 
+      // render
+      renderer.render( scene, camera );
       var animate = function () {
         requestAnimationFrame( animate );
-
-        if (index < 1000){
-          walks[0].push(
-            [walks[0].slice(-1)[0][0]+1, walks[0].slice(-1)[0][1]+ boxMullerRandom()]
-          );
-          walks[1].push(
-            [walks[1].slice(-1)[0][0]+1, walks[1].slice(-1)[0][1]+ boxMullerRandom()]
-          );
-          walks[2].push(
-            [walks[2].slice(-1)[0][0]+1, walks[2].slice(-1)[0][1]+ boxMullerRandom()]
-          );
-          walks[3].push(
-            [walks[3].slice(-1)[0][0]+1, walks[3].slice(-1)[0][1]+ boxMullerRandom()]
-          );
-          walks[4].push(
-            [walks[4].slice(-1)[0][0]+1, walks[4].slice(-1)[0][1]+ boxMullerRandom()]
-          );
-          walks[5].push(
-            [walks[5].slice(-1)[0][0]+1, walks[5].slice(-1)[0][1]+ boxMullerRandom()]
-          );
-
-          points1.push(new THREE.Vector3(walks[0][index][0]-50, walks[0][index][1], 0));
-          points2.push(new THREE.Vector3(walks[1][index][0]-50, walks[1][index][1], 0));
-          points3.push(new THREE.Vector3(walks[2][index][0]-50, walks[2][index][1], 0));
-          points4.push(new THREE.Vector3(walks[3][index][0]-50, walks[3][index][1], 0));
-          points5.push(new THREE.Vector3(walks[4][index][0]-50, walks[4][index][1], 0));
-          points6.push(new THREE.Vector3(walks[5][index][0]-50, walks[5][index][1], 0));
+        if(index > (window.innerWidth/2)){
+          return;
         }
-
-        // if ((index % 500)===0){
-        //   add = -1 * add;
-        // }
-        // line1.rotation.y += add;
-        // line1.rotation.x += add;
-        // line2.rotation.y += add;
-        // line2.rotation.x += add;
-        // line3.rotation.y += add;
-        // line3.rotation.x += add;
-        // line4.rotation.y += add;
-        // line4.rotation.x += add;
-        // line5.rotation.y += add;
-        // line5.rotation.x += add;
-        // line6.rotation.y += add;
-        // line6.rotation.x += add;
-
-        if (index < 1000){
-          geometry1.setFromPoints(points1);
-          // _line1.setGeometry(geometry1);
-          geometry2.setFromPoints(points2);
-          // _line2.setGeometry(geometry2);
-          geometry3.setFromPoints(points3);
-          // _line3.setGeometry(geometry3);
-          geometry4.setFromPoints(points4);
-          // _line4.setGeometry(geometry4);
-          geometry5.setFromPoints(points5);
-          // _line5.setGeometry(geometry5);
-          geometry6.setFromPoints(points6);
-          // _line6.setGeometry(geometry6);
-          renderer.render( scene, camera );
-          index++;
-        }
-    };
+        line1.material.uniforms.index.value = index;
+        // line2.material.uniforms.index.value = index;
+        // line3.material.uniforms.index.value = index;
+        // line4.material.uniforms.index.value = index;
+        // line5.material.uniforms.index.value = index;
+        // line6.material.uniforms.index.value = index;
+        index+=4;
+        renderer.render( scene, camera );
+      };
       animate();
       // === THREE.JS EXAMPLE CODE END ===
     }
